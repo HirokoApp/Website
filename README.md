0f25d55f8423dea99643bfeff3b2ac99f5b88fa6
TheRealToxicDev <toxic.dev09@gmail.com>
2024-05-15 00:21:49 -0600

    fix(app): added redirects, and fixed some issues

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## Hiroko Website
-Website/Landing page for Hiroko Bot!
+Just another simple, customizable and sometimes witty discord bot, here to help you manage and moderate your discord servers with ease.
+
+---
+
+### Notice
+- This source-code does not contain and parts of our Dashboard or API code!
+- Open-Source primarily for transparency purposes, self hosting not recommended!
--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    compress: true,
+    swcMinify: true,
+    optimizeFonts: true,
+    productionBrowserSourceMaps: false,
+    reactStrictMode: true,
+    cleanDistDir: true,
+    async redirects() {
+        return [{
+            source: '/discord',
+            destination: 'https://discord.gg/4DMmqYESXf',
+            permanent: true
+        },{
+            source: '/twitter',
+            destination: 'https://twitter.com/HeyyHiroko',
+            permanent: true
+        },{
+            source: '/github',
+            destination: 'https://github.com/HirokoApp',
+            permanent: true
+        }]
+    }
+}
 
 module.exports = nextConfig
